d9797db4792bf224e6f464bfc65a21b58921205c
0ishan0 <ishanbagchi4@gmail.com>
2020-05-08 17:08:40 +0530

    Edit app.js

--- webdevbootcamp/introToExpress/app.js
+++ webdevbootcamp/introToExpress/app.js
@@ -6,6 +6,16 @@ app.get("/", function(req, res){
     res.send("Hi There!");
 });
 
+// "/bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+// "/dog" => "MEAO!!!"
+app.get("/dog", function(req, res){
+    res.send("MEAO!!!");
+});
+
 //Tell express to listen for requests (start server)
 
 app.listen(3000, function(){
