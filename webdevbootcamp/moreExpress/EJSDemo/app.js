825dfd7940fce22ab68359b3517a5e3df2d4243b
0ishan0 <ishanbagchi4@gmail.com>
2020-05-09 00:51:54 +0530

    Learn about loop and array in .ejs

--- webdevbootcamp/moreExpress/EJSDemo/app.js
+++ webdevbootcamp/moreExpress/EJSDemo/app.js
@@ -11,6 +11,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My addorable pet Jimmy", author: "Iman"},
+        {title: "Why do you love Panda?", author: "Ishan"}
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(3000, function(){
     console.log("Your app is running in localhost:3000");
 });
