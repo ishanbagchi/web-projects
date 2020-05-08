74be3b0ff75e7b2671b420d8a6b809beca87d9c9
0ishan0 <ishanbagchi4@gmail.com>
2020-05-08 18:51:43 +0530

    Learn different routs

--- webdevbootcamp/introToExpress/app.js
+++ webdevbootcamp/introToExpress/app.js
@@ -13,7 +13,23 @@ app.get("/bye", function(req, res){
 
 // "/dog" => "MEAO!!!"
 app.get("/dog", function(req, res){
+    console.log("Someone made a request to /dog");
     res.send("MEAO!!!");
+});
+
+app.get("/r/:subName", function(req, res){
+    var sName = req.params.subName;
+    res.send("WELCOME TO THE " + sName.toUpperCase() + " SUB PAGE!");
+});
+
+app.get("/r/:subName/comments/:id/:title", function(req, res){
+    console.log(req.params);
+    res.send("Welcome to the comments page!!!");
+});
+
+//ERROR PAGE
+app.get("*", function(req, res){
+    res.send("YOU ARE A STAR!!!");
 });
 
 //Tell express to listen for requests (start server)
