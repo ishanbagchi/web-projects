b9ab640413a9297dfe1da9eb53a4fc5ef9681cd7
0ishan0 <ishanbagchi4@gmail.com>
2020-05-09 16:12:50 +0530

    Finsh result route

---
+++ webdevbootcamp/moreExpress/MovieSearchApp/app.js
@@ -0,0 +1,25 @@
+//General search: http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb
+//Search with movie id: http://www.omdbapi.com/?i=tt3896198&apikey=thewdb
+
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/results", function(req, res){
+    request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var results = JSON.parse(body);
+            res.send(results["Search"][0]["Title"]);
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Your app is running in localhost:3000");
+});
