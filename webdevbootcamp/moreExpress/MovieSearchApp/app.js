86bac4a5cfc11e3f90c52ca8cd01291d4a221a73
0ishan0 <ishanbagchi4@gmail.com>
2020-05-09 18:46:15 +0530

    Conclude movie search app

--- webdevbootcamp/moreExpress/MovieSearchApp/app.js
+++ webdevbootcamp/moreExpress/MovieSearchApp/app.js
@@ -11,8 +11,14 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+app.get("/search", function(req, res){
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=guardian&apikey=thewdb", function(error, response, body){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             res.render("results", {data: data});
